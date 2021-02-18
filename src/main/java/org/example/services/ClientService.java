880b0e17074ecaaa53ad8d9b4840600f9b7edb2d
Luiz.Dorici <luizdorici@gmail.com>
2021-02-18 11:50:22 -0300

    Client PUT, DELETE and GET methods added

--- src/main/java/org/example/services/ClientService.java
+++ src/main/java/org/example/services/ClientService.java
@@ -1,11 +1,19 @@
 package org.example.services;
 
 import org.example.domain.Client;
+import org.example.domain.Client;
+import org.example.dto.ClientDTO;
 import org.example.repositories.ClientRepository;
+import org.example.services.exceptions.DataIntegrityException;
 import org.example.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,9 +26,42 @@ public class ClientService {
         this.repository = repository;
     }
 
+    public List<Client> findAll() {
+        return repository.findAll();
+    }
+
+    public Page<Client> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        return repository.findAll(pageRequest);
+    }
+
     public Client findById(Integer id) {
         Optional<Client> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Object not found! Id: " + id + ", Type: " + Client.class.getName()));
+    }
+
+    public Client update(Client obj) {
+        Client newObj = findById(obj.getId());
+        updateData(newObj, obj);
+        return repository.save(newObj);
+    }
+
+    public void delete(Integer id) {
+        findById(id);
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Not possible to exclude a client with related entities");
+        }
+    }
+
+    public Client fromDTO(ClientDTO objDto) {
+        return new Client(objDto.getId(), objDto.getName(), objDto.getEmail(), null, null);
+    }
+
+    private void updateData(Client newObj, Client obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
     }
 }
