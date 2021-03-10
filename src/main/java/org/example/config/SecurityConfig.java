6c79157c17ae3ce9a4b71cdfacfbb9a32707e7d1
Luiz <luizdorici@gmail.com>
2021-03-10 10:18:48 -0300

    endpoint authorization filter impl

--- src/main/java/org/example/config/SecurityConfig.java
+++ src/main/java/org/example/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.example.config;
 
 import org.example.security.JWTAuthenticationFilter;
+import org.example.security.JWTAuthorizationFilter;
 import org.example.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
